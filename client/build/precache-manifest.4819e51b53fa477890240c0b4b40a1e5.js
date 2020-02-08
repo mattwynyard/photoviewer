e4abb725fd4e3ab4707780f256a683804697ecfd
matt <mjwynyard@gmail.com>
2020-02-09 12:05:14 +1300

    working version 1.0.0

---
+++ client/build/precache-manifest.4819e51b53fa477890240c0b4b40a1e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4817894271c8642069730e6902c08da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da5e7d5b838d495d6bcf",
+    "url": "/static/css/2.05a206a1.chunk.css"
+  },
+  {
+    "revision": "e006e7423a7a65341ce0",
+    "url": "/static/css/main.ed6bf1f4.chunk.css"
+  },
+  {
+    "revision": "da5e7d5b838d495d6bcf",
+    "url": "/static/js/2.b170d9b0.chunk.js"
+  },
+  {
+    "revision": "25e13315a3050b9fb3a7914170a25f82",
+    "url": "/static/js/2.b170d9b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e006e7423a7a65341ce0",
+    "url": "/static/js/main.e0aeb49e.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
