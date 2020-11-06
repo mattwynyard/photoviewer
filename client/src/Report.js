85094bc699a8662c8a844f4ce8d025835d27086e
Matt <mjwynyard@gmail.com>
2020-11-06 14:58:44 +1300

    working charts

--- client/src/Report.js
+++ client/src/Report.js
@@ -28,9 +28,13 @@ class Report extends React.Component {
             'rgba(255, 99, 132, 1)',
             'rgba(255, 206, 86, 1)',
             'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            
+            'rgba(153, 102, 255, 1)',          
         ];
+    }
+
+    clickChart(e) {
+        //console.log(e.target);
+        console.log(this.gradeChart.getElementsAtEvent(e));
     }
 
     componentDidMount() {
@@ -40,7 +44,6 @@ class Report extends React.Component {
         this.g2Map = new Map();
         this.g3Map = new Map();
         this.data.forEach(item => {
-
             if (item.priority !== 0 ) {
                 if (item.priority === 99) {
                     item.priority = "Signage";
@@ -52,37 +55,55 @@ class Report extends React.Component {
                 case  "Priority: 1":
                     this.g1Map = this.addMap(this.g1Map, item.fault);
                 case  "Priority: 2":
-                    this.g2Map = this.addMap(this.g3Map, item.fault);
+                    this.g2Map = this.addMap(this.g2Map, item.fault);
                 case  "Priority: 3":
                     this.g3Map = this.addMap(this.g3Map, item.fault);
             }
             this.gradeMap = this.addMap(this.gradeMap, item.priority);
             this.faultMap = this.addMap(this.faultMap, item.class);
-
         });
         let gradeData = Array.from(this.gradeMap, ([name, value]) => ({ name, value }));
         let faultData = Array.from(this.faultMap, ([name, value]) => ({ name, value }));
         let g1Data = Array.from(this.g1Map, ([name, value]) => ({ name, value }));
-        
+        let g2Data = Array.from(this.g2Map, ([name, value]) => ({ name, value }));
+        let g3Data = Array.from(this.g3Map, ([name, value]) => ({ name, value }));
         gradeData.sort((a, b) => (a.value < b.value) ? 1 : -1);
         faultData.sort((a, b) => (a.value < b.value) ? 1 : -1);
         g1Data.sort((a, b) => (a.value < b.value) ? 1 : -1);
+        g2Data.sort((a, b) => (a.value < b.value) ? 1 : -1);
+        g3Data.sort((a, b) => (a.value < b.value) ? 1 : -1);
+        console.log(g2Data)
         let g1Top = g1Data.slice(0, 5);
         let g1Bottom = g1Data.slice(5, g1Data.length);
-
-        let value = 0;
+        let g2Top = g2Data.slice(0, 5);
+        let g2Bottom = g2Data.slice(5, g2Data.length);
+        let g3Top = g3Data.slice(0, 5);
+        let g3Bottom = g3Data.slice(5, g3Data.length);
+        let value1 = 0;
+        let value2 = 0;
+        let value3 = 0;
         g1Bottom.forEach(item => {
-            value += item.value;
+            value1 += item.value;
         });
-        let others = {name: "Others", value};
-        g1Top.push(others);
-        console.log(g1Top);
+        g2Bottom.forEach(item => {
+            value2 += item.value;
+        });
+        g3Bottom.forEach(item => {
+            value3 += item.value;
+        });
+        let others1 = {name: "Others", value: value1};
+        let others2 = {name: "Others", value: value2};
+        let others3 = {name: "Others", value: value3};
+        g1Top.push(others1);
+        g2Top.push(others2);
+        g3Top.push(others3);
+       
         var ctx = document.getElementById('myChart').getContext("2d");
         let ct = this.buildColorTable(g1Top.length);
-        var gradeChart = new Chart(ctx, {
+        this.gradeChart = new Chart(ctx, {
             type: 'doughnut',
             data: {
-                labels: gradeData.map((gradeData) => gradeData.name),
+                labels: gradeData.map((gradeData) => gradeData.name + ": " + gradeData.value),
                 datasets: [{
                     data: gradeData.map((gradeData) => gradeData.value),
                     backgroundColor: [
@@ -101,31 +122,27 @@ class Report extends React.Component {
                 }]
             },
             options: {
-                
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            display: false
-                        }
-                        
-                    }]
+                onClick: (e) => {
+                    this.clickChart(e);
                 },
                 maintainAspectRatio: false,
                 legend: {
-                    position: 'bottom',
+                    position: 'right',
+
                     labels: {
+                        padding: 10,
                         boxWidth: 12
                     }
                 }
             }
         });
 
-        var ctx = document.getElementById("class").getContext('2d');
-
-            var g1Chart = new Chart(ctx, {
-                type: 'doughnut',
+        var ctx = document.getElementById("grade1").getContext('2d');
+        console.log(g1Top);
+        var g1Chart = new Chart(ctx, {
+            type: 'doughnut',
             data: {
-                labels: g1Top.map((g1Top) => g1Top.name),
+                labels: g1Top.map((g1Top) => g1Top.name + ": " + g1Top.value),
                 datasets: [{
                     data: g1Top.map((g1Top) => g1Top.value),
                     backgroundColor: [
@@ -134,8 +151,7 @@ class Report extends React.Component {
                         'rgba(255, 99, 132, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        
+                        'rgba(153, 102, 255, 1)',                  
                     ],
                     borderColor: [
                         'rgba(255, 159, 64, 1)',
@@ -143,25 +159,109 @@ class Report extends React.Component {
                         'rgba(255, 99, 132, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        
+                        'rgba(153, 102, 255, 1)',                 
                     ],
                     borderWidth: 1
                 }]
             },
             options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            display: false
-                        }
-                        
-                    }]
+                title: {
+                    display: true,
+                    position: 'top',
+                    text: 'Priority 1',
                 },
                 maintainAspectRatio: false,
                 legend: {
-                    position: 'bottom',
+                    position: 'right',
+                    fullWidth: false,
                     labels: {
+                        padding: 10,
+                        boxWidth: 12
+                    }
+                }
+            }
+        });
+
+        var ctx = document.getElementById("grade2").getContext('2d');
+        var g2Chart = new Chart(ctx, {
+            type: 'doughnut',
+        data: {
+            labels: g2Top.map((g2Top) => g2Top.name + ": " + g2Top.value),
+            datasets: [{
+                data: g2Top.map((g2Top) => g2Top.value),
+                backgroundColor: [
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',             
+                ],
+                borderColor: [
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',                   
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Priority 2',
+            },
+            maintainAspectRatio: false,
+            legend: {
+                position: 'right',
+                labels: {
+                    padding: 10,
+                    boxWidth: 12
+                }
+            }
+        }
+        });
+
+        var ctx = document.getElementById("grade3").getContext('2d');
+            var g3Chart = new Chart(ctx, {
+                type: 'doughnut',
+            data: {
+                labels: g3Top.map((g3Top) => g3Top.name + ": " + g3Top.value),
+                datasets: [{
+                    data: g3Top.map((g3Top) => g3Top.value),
+                    backgroundColor: [
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',            
+                    ],
+                    borderColor: [
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',                     
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Priority 3',
+                    padding: 5,
+                },
+                maintainAspectRatio: false,
+                responsive: true,
+                legend: {
+                    position: 'right',
+                    labels: {
+                        padding: 10,
                         boxWidth: 12
                     }
                 }
@@ -189,19 +289,26 @@ class Report extends React.Component {
               alt="logo"
             />
           </Navbar.Brand>
-
           <Nav> 
             <Link className="dropdownlink" to={'/'} style={{ textDecoration: 'none' }}>Home</Link>
             </Nav>
             <CustomNav title={this.user}></CustomNav>
             </Navbar>
             
-            <div className="faultChart">
-            <canvas id="class"></canvas>  
-            </div>
-            <div className="priorityChart">
-            <canvas id="myChart"></canvas>  
-            </div>
+            <div className="chartParent">
+                <div className = "g1div">
+                    <canvas className="g1Chart" id="grade1"></canvas>  
+                </div>
+                <div className = "g2div">
+                    <canvas className="g2Chart" id="grade2"></canvas>  
+                </div>
+                <div className = "g3div">
+                    <canvas className="g3Chart" id="grade3"></canvas>  
+                </div>
+                <div className = "gradediv">
+                    <canvas className="gradeChart" id="myChart"></canvas>  
+                </div>
+            </div>    
       </div> 
     );
     }
--- server/src/app.js
+++ server/src/app.js
@@ -40,8 +40,6 @@ if(environment === 'production') {
     });
 }
 
-
-
 app.use(cors());
 app.use(morgan('dev'));
 app.use(helmet());
@@ -378,7 +376,7 @@ app.post('/dropdown', async (req, res) => {
 app.post('/layer', async (req, res) => {
   const result = users.findUserToken(req.headers.authorization, req.body.user);
   if (result) {
-    //console.log(req.body);
+    console.log(req.body);
     let filterObj = req.body.filterObj;
     let project = req.body.project;
     let filter = req.body.filter;
@@ -432,7 +430,6 @@ app.post('/layer', async (req, res) => {
       await db.updateFilterCount(project);
     }  
     res.set('Content-Type', 'application/json');
-    //console.log(finalGeometry);
     res.send({type: surface.rows[0].surface, geometry: finalGeometry});
   } else {
     res.send({error: "Invalid token"});
