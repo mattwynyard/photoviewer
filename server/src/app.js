17003b282a7f4ba565b751ff262fefdbb05cca08
Matt <mjwynyard@gmail.com>
2021-07-27 09:40:58 +1200

    working version 1.4 - thick lines on zoom

--- server/src/app.js
+++ server/src/app.js
@@ -22,22 +22,22 @@ const environment = process.env.ENVIRONMENT;
 
 // comment out create server code below when deploying to server
 // server created in index.js
-const options = {
-  key: fs.readFileSync('./server.key', 'utf8'),
-  cert: fs.readFileSync('./server.cert', 'utf8')
-}
-console.log("mode: " + environment);
-if(environment === 'production') {
-  let hostname = "localhost";
- http.createServer(function(req, res) {
-  }).listen(port, hostname, () => {
-      console.log(`Listening: http://${hostname}:${port}`);
-   });
-} else {
-  https.createServer(options, app).listen(port, () => {
-    console.log(`Listening: https://${host}:${port}`);
-    });
-}
+// const options = {
+//   key: fs.readFileSync('./server.key', 'utf8'),
+//   cert: fs.readFileSync('./server.cert', 'utf8')
+// }
+// console.log("mode: " + environment);
+// if(environment === 'production') {
+//   let hostname = "localhost";
+//  http.createServer(function(req, res) {
+//   }).listen(port, hostname, () => {
+//       console.log(`Listening: http://${hostname}:${port}`);
+//    });
+// } else {
+//   https.createServer(options, app).listen(port, () => {
+//     console.log(`Listening: https://${host}:${port}`);
+//     });
+// }
 
 app.use(cors());
 app.use(morgan('dev'));
