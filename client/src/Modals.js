5e39c05984fb48c43aabf0c693b03d6f5ea2db0d
mjwynyard@gmail.com <mjwynyard@gmail.com>
2021-11-24 11:13:07 +1300

    Online version 2.3

--- client/src/Modals.js
+++ client/src/Modals.js
@@ -38,7 +38,7 @@ export default function Modals(props) {
         <Modal.Title><h2>About</h2> </Modal.Title>
       </Modal.Header>
       <Modal.Body >	
-        <b>Road Inspection Version 2.2</b><br></br>
+        <b>Road Inspection Version 2.3</b><br></br>
         Relased: 23/04/2020<br></br>
         Company: Onsite Developments Ltd.<br></br>
         Software Developer: Matt Wynyard <br></br>
