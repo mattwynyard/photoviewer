6e4acd30caf96f061fe4acfe678071e4919361ac
mjwynyard@gmail.com <mjwynyard@gmail.com>
2022-10-26 08:30:10 +1300

    popup

--- client/src/components/DefectPopup.jsx
+++ client/src/components/DefectPopup.jsx
@@ -3,6 +3,57 @@ import { Popup }  from 'react-leaflet';
 import { Image }  from 'react-bootstrap';
 import { useEffect } from 'react';
 
+
+//not working yet
+const Thumbnail = ({amazon, data, login, photo, onError, onClick}) => {
+  if (login === "asu" || login === "asm") {
+    return (<Image 
+              className="thumbnail" 
+              src={`${amazon}${data.inspection}/${photo}.jpg`}
+              onClick={onClick} 
+              thumbnail={true}
+              onError={onError}
+                >
+              </Image >
+            );
+      }
+  if ((data.type === 'footpath')) {
+    if (data.asset.toLowerCase() === "kerb & channel") {
+      return (
+        <Image 
+          className="thumbnail" 
+          src={`${amazon}kerbs/${data.inspection}/${photo}.jpg`}
+          onClick={onClick} 
+          thumbnail={true}
+          onError={onError}
+          >
+        </Image >
+      );
+    } else {
+      return (
+        <Image 
+          className="thumbnail" 
+          src={`${amazon}footpaths/${data.inspection}/${photo}.jpg`}
+          onClick={onClick} 
+          thumbnail={true}
+          onError={onError}
+          >
+        </Image >
+      );
+    }
+  }
+  return (
+    <Image 
+      className="thumbnail" 
+      src={`${amazon}/${photo}.jpg`}
+      onClick={onClick} 
+      thumbnail={true}
+      onError={onError}
+      >
+    </Image >
+  );
+}
+
 const DefectPopup = (props) => {
 
     const [prefix, setPrefix] = useState(null);
@@ -32,6 +83,14 @@ const DefectPopup = (props) => {
               <b>{"Date: "}</b>{props.data.datetime} 
             </p>
             <div>
+              {/* <Thumbnail
+                amazon={props.amazon}
+                data={props.data}
+                login={props.login}
+                onError={props.onError}
+                onClick={props.onClick}
+                photo={props.photo}
+              /> */}
               <Image className="thumbnail" 
                 src={`${prefix}${props.photo}.jpg`}
                 onClick={props.onClick} 
